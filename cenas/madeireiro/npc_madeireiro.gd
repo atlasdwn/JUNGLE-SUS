0da84155a9cc5f4f8bd8ef3ef906fcac055c966a
Yago <yagomauricio.lima@gmail.com>
2026-04-04 13:58:37 -0400

    subst player mock p/ carina

--- cenas/madeireiro/npc_madeireiro.gd
+++ cenas/madeireiro/npc_madeireiro.gd
@@ -9,7 +9,6 @@ var player_in_area = false
 var falando = false
 var pode_avancar = false
 var fala_index = 0
-
 var falas = [
 	"Ola, voce esta perdida? vc eh do ibama?"
 ]
@@ -19,7 +18,7 @@ func _ready() -> void:
 	caixa_dialogo.visible = false
 	texto_dialogo.visible = false
 	label_interacao.visible = false
-	
+
 func _process(_delta: float) -> void:
 	if player_in_area and not falando and Input.is_action_just_pressed("interact"):
 		iniciar_dialogo()
@@ -31,11 +30,11 @@ func _process(_delta: float) -> void:
 
 
 func _on_body_entered(body) -> void:
-	if body.name == "PlayerMock":
+	if body.name == "Carina":
 		player_in_area = true
 		label_interacao.text = "Pressione 'E' para interagir"
 		label_interacao.visible = true
-		
+
 
 
 
@@ -45,7 +44,7 @@ func _on_body_exited(body) -> void:
 		label_interacao.visible = false
 		if falando:
 			encerrar_dialogo()
-	
+
 
 func iniciar_dialogo():
 	falando = true
@@ -54,7 +53,7 @@ func iniciar_dialogo():
 	texto_dialogo.visible = true
 	fala_index = 0
 	proxima_fala()
-	
+
 func proxima_fala():
 	if fala_index < falas.size():
 		pode_avancar = false
@@ -64,15 +63,15 @@ func proxima_fala():
 		mostrar_texto_com_efeito(texto)
 	else:
 		encerrar_dialogo()
-		
-		
+
+
 func mostrar_texto_com_efeito(texto: String):
 	await get_tree().create_timer(0.1).timeout
 	for letra in texto:
 		texto_dialogo.text += letra
 		await get_tree().create_timer(0.02).timeout
 	pode_avancar = true
-	
+
 func encerrar_dialogo():
 	falando = false
 	pode_avancar = false
