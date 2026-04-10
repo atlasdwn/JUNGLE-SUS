dce5f2c8c79513675fa97b78ee47748f083b6603
Yago <yagomauricio.lima@gmail.com>
2026-04-10 08:05:13 -0400

    mov. diagonal normalizada

--- cenas/carina/scripts/state_walk.gd
+++ cenas/carina/scripts/state_walk.gd
@@ -16,7 +16,7 @@ func process(_delta: float) -> State:
 	if player.direction == Vector2.ZERO:
 		return idle
 	
-	player.velocity = player.direction * move_speed
+	player.velocity = player.direction.normalized() * move_speed
 	
 	if player.set_direction():
 		player.update_animation("walk")
