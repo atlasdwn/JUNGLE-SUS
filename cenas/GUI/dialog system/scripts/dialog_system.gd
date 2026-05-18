9ddf9bb114044c6aaba2f5a684075c0db30b79d3
Yago Lima <yagomauricio.lima@gmail.com>
2026-05-18 13:42:44 -0400

    criação visual dialogsys e pasta menu movimentado pra GUI

--- cenas/GUI/dialog system/scripts/dialog_system.gd
+++ cenas/GUI/dialog system/scripts/dialog_system.gd
@@ -5,9 +5,9 @@ class_name DialogSystemNode extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		if get_viewport() is Window:
+			get_parent().remove_child(self)
+			return
+		return
 	pass
